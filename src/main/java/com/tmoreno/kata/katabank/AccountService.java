b2e0330f502ca5a1748d754716b0a656322b1acd
tmoreno <tmorenobernal@yahoo.es>
2015-03-19 12:15:22 +0100

    The signature of this class can't be modified

---
+++ src/main/java/com/tmoreno/kata/katabank/AccountService.java
@@ -0,0 +1,16 @@
+package com.tmoreno.kata.katabank;
+
+public class AccountService {
+
+	public void deposit(int amount) {
+
+	}
+
+	public void withdraw(int amount) {
+
+	}
+
+	public void printStatement() {
+
+	}
+}
